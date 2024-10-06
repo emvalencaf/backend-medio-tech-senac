daaaf1247850a8cd667a3dff6deef66213787ef1
emvalencaf <edsonmvf@gmail.com>
2024-10-06 19:41:22 -0300

    :wrench: fix: Fix Redis for production

--- src/redis/redis.provider.ts
+++ src/redis/redis.provider.ts
@@ -7,6 +7,8 @@ export const redisProvider = {
         console.log('URL do Redis:', process.env.REDIS_URL);
         console.log('Host do Redis:', process.env.REDIS_HOST);
         console.log('Porta do Redis:', process.env.REDIS_PORT);
+        console.log('Usuário do Redis: ', process.env.REDIS_USERNAME);
+        console.log('Senha do Redis:', process.env.REDIS_PASSWORD);
 
         try {
             if (process.env.ENVIRONMENT === 'DEVELOPMENT') {
@@ -19,8 +21,14 @@ export const redisProvider = {
                     port,
                 });
             } else {
-                return new Redis(process.env.REDIS_URL, {
-                    tls: { rejectUnauthorized: false },
+                return new Redis({
+                    host: process.env.REDIS_HOST || '',
+                    port: Number(process.env.REDIS_PORT),
+                    username: process.env.REDIS_USERNAME || '',
+                    password: process.env.REDIS_PASSWORD || '',
+                    tls: {
+                        rejectUnauthorized: false, // Para permitir a conexão sem verificar o certificado
+                    },
                 });
             }
         } catch (error) {
