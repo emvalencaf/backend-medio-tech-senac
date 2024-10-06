c8555c9c5a0f875253aa57fa129b26c092559460
emvalencaf <edsonmvf@gmail.com>
2024-10-06 16:29:46 -0300

    :wrench: fix: Fix Redis for production

--- src/redis/redis.provider.ts
+++ src/redis/redis.provider.ts
@@ -1,19 +1,14 @@
 import Redis from 'ioredis';
 
-const redisConfig =
-    process.env.ENVIRONMENT === 'DEVELOPMENT'
-        ? {
-              host: process.env.REDIS_HOST || 'localhost',
-              port: parseInt(process.env.REDIS_PORT, 10) || 6379,
-          }
-        : process.env.REDIS_URL; // A URL já será uma string no ambiente de produção
-
 export const redisProvider = {
     provide: 'REDIS',
     useFactory: () => {
         // Checa se é um objeto ou string para instanciar Redis corretamente
-        return typeof redisConfig === 'string'
-            ? new Redis(redisConfig) // Para produção, utilizando URL
-            : new Redis(redisConfig); // Para desenvolvimento, utilizando host e port
+        return process.env.ENVIRONMENT === 'DEVELOPMENT'
+            ? new Redis({
+                  host: process.env.REDIS_HOST || 'localhost',
+                  port: Number(process.env.REDIS_PORT) || 6379,
+              }) // Para produção, utilizando URL
+            : new Redis(process.env.REDIS_URL); // Para desenvolvimento, utilizando host e port
     },
 };
